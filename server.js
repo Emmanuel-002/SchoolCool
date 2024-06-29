5cc92984b08e150dfeccb963324695bdfd1a862b
Emmanuel-002 <segseyman8@gmail.com>
2024-06-29 21:49:16 +0100

    First Deployment

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from 'express'
-import { join, dirname } from "path";
+import { join, dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import Routes from './routes/route.js'
+import Routes from './routes/index.js'
 const app = express()
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config();
 app.use(cors())
-app.use(express.static(join(__dirname, "public", "app")));
+app.use(express.static(resolve(__dirname, "public/app")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: '10mb' }))
 
@@ -24,7 +24,10 @@ mongoose
     .then(data=>console.log(data?"connected to mongodb":""))
     .catch((err) =>console.log(`${err.code} ${err.hostname}`))
 
-app.use('/', Routes);
+app.use('/api', Routes);
+
+app.get("*", (req, res) => res.sendFile(resolve(__dirname, "public/app", "index.html")));
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started at port no. ${process.env.PORT}`)
